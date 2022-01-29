bead1b095bc81e5fa7c4b67d74986e1704f49c8b
WilliamBlack99 <iamwill55@hotmail.com>
2022-01-28 19:39:40 -0800

    pollution and money counter

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ size = width, height = 400, 600
 screen = pygame.display.set_mode(size)
 
 group = pygame.sprite.RenderPlain()
-group.add(clicker.Clicker())
+my_clicker = clicker.Clicker()
+group.add(my_clicker)
+
+pollution = 0
+
+font = pygame.font.Font(None, 16)
 
 
 while True:
@@ -18,7 +23,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+
+            clicked_sprites = [s for s in group if s.rect.collidepoint(pos)]
+
+            for sprite in clicked_sprites:
+                pollution += sprite.click()
 
     screen.fill((100,100,100))
+    pollution_text = font.render("Pollution: " + str(pollution), True, (0,0,0))
+    pollution_text_rect = pygame.Rect(0,400, 100, 100)
+    money_text = font.render("$" + str(my_clicker.money), True, (0,0,0))
+    money_text_rect = pygame.Rect(0, 500, 100, 100)
+    
+    screen.blit(pollution_text, (0, 400))
+    screen.blit(money_text, (0, 500))
+    
     group.draw(screen)
     pygame.display.update()
+
+    print(pollution)
