0213ebe4b4c417d3748a26b4052752e90db11714
dirksen-argon <96699346+dirksen-argon@users.noreply.github.com>
2022-01-28 19:01:59 -0800

    this

--- clicker.py
+++ clicker.py
@@ -1,13 +1,13 @@
-if __name__ != __main__:
+if __name__ != "__main__":
     import pygame
 
-    class Clicker(pygame.Sprite):
+    class Clicker(pygame.sprite.Sprite):
         def __init__(self):
             super().__init__()
             self.money = 0
             self.image = pygame.image.load("dollar.png")
             self.rect = pygame.Rect((0,0), (100,200))
-            self.rect.center = (100, 200)
+            self.rect.center = (50, 100)
         def click(self):
             self.money = self.money + 1
             return -1
