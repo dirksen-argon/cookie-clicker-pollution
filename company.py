c097d694e143cf37f259c52bd5cd00cd832ce684
dirksen-argon <96699346+dirksen-argon@users.noreply.github.com>
2022-01-29 08:22:44 -0800

    updated several files

--- company.py
+++ company.py
@@ -8,14 +8,14 @@ if __name__ != "__main__":
         location = ((50, 50), (50, 100), (100, 100), (50, 150), (50, 200), \
                     (50, 250), (50, 300), (50, 350))
         def __init__(self):
-            start = time.time()
+            self.start = time.time()
             self.image = pygame.image.load("factory.png")
             self.image = pygame.transform.scale(self.image, (40, 40))
             self.rect = self.image.get_rect()
             self.rect.center = random.choice(location)
         def click(self):
             end = time.time()
-            house = int(end) - int(start)
+            house = int(end) - int(self.start)
             if house >= 3:
                 start = time.time()
                 factory = generator.Generator()
--- generator.py
+++ generator.py
@@ -3,16 +3,17 @@ if __name__ != "__main__":
     import time
 
     class Generator:
-        def __init__(self, num, pol):
-            start = time.time()
+        def __init__(self, num, pol = 0, money = 0):
+            self.start = time.time()
             self.pol = pol
             self.num = num
+            self.money = money
         def add(self):
             end = time.time()
-            house = int(end) - int(start)
+            house = int(end) - int(self.start)
             if house >= self.num:
-                start = time.time()
-                return self.pol
+                self.start = time.time()
+                return self.pol, self.money
             else:
                 return 0
 else:
