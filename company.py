b4ce15eee47f226ef5a31f073498ad2c024ea0ba
dirksen-argon <96699346+dirksen-argon@users.noreply.github.com>
2022-01-29 08:58:04 -0800

    Update company.py

--- company.py
+++ company.py
@@ -4,15 +4,13 @@ if __name__ != "__main__":
     import generator
     import random
 
-    class Company:
-        location = ((50, 50), (50, 100), (100, 100), (50, 150), (50, 200), \
-                    (50, 250), (50, 300), (50, 350))
-        def __init__(self):
+    class Company(pygame.sprite.Sprite):
+        def __init__(self, x, y):
             self.start = time.time()
             self.image = pygame.image.load("factory.png")
             self.image = pygame.transform.scale(self.image, (40, 40))
             self.rect = self.image.get_rect()
-            self.rect.center = random.choice(Company.location)
+            self.rect.center = (x, y)
         def tick(self):
             end = time.time()
             house = int(end) - int(self.start)
@@ -22,5 +20,9 @@ if __name__ != "__main__":
                 return factory
             else:
                 return None
+        def get_progress(self):
+            end = time.time()
+            house = (end - self.start)/3
+            return house
 else:
     print("on the wall")
