f8efb4bc26e85bc70d4b31d95f0fd2c2e2f98433
dirksen-argon <96699346+dirksen-argon@users.noreply.github.com>
2022-01-29 09:10:49 -0800

    Update company.py

--- company.py
+++ company.py
@@ -10,7 +10,7 @@ if __name__ != "__main__":
             self.image = pygame.image.load("factory.png")
             self.image = pygame.transform.scale(self.image, (40, 40))
             self.rect = self.image.get_rect()
-            self.rect.center = (x, y)
+            self.rect.topleft = (x, y)
         def tick(self):
             end = time.time()
             house = int(end) - int(self.start)
