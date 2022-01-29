9ce7dce211314faa416afae3f80290b2517745c3
dirksen-argon <96699346+dirksen-argon@users.noreply.github.com>
2022-01-28 19:00:18 -0800

    cliiiiiiker

--- clicker.py
+++ clicker.py
@@ -6,7 +6,7 @@ if __name__ != __main__:
             super().__init__()
             self.money = 0
             self.image = pygame.image.load("dollar.png")
-            self.rect = self.image.get_rect()
+            self.rect = pygame.Rect((0,0), (100,200))
             self.rect.center = (100, 200)
         def click(self):
             self.money = self.money + 1
